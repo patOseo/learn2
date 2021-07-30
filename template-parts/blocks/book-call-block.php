1f25c6f4182c84bf4846138b043848b209f5bf18
Pat Monette <patrick@ontarioseo.ca>
2021-07-30 18:52:13 +0000

    book call block update

--- template-parts/blocks/book-call-block.php
+++ template-parts/blocks/book-call-block.php
@@ -35,11 +35,13 @@ $background_color = get_field('background_color');
     <div class="container">
         <?= $description; ?>
         <div class="row justify-content-center mt-5">
+            <?php if($vedio_link): ?>
             <div class="col-md-9">
                 <div class="yt-place shadow">
                     <iframe width="560" height="315" src="<?= $vedio_link; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
+            <?php endif; ?>
             <div class="col-12 mt-4 text-center">
                 <a href="<?= $button_link; ?>" class="align-items-center bigger cta d-inline-flex justify-content-center mt-4 orangegrad px-5 py-3 text-uppercase text-white"><?= $button; ?></a>
             </div>
