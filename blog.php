380ae0080eac75eeab72248c76b3e31f6d2b4a35
Pat Monette <patrick@ontarioseo.ca>
2021-08-12 19:54:10 +0000

    blog default image

--- blog.php
+++ blog.php
@@ -43,7 +43,7 @@ get_header();
 		                    <div class="copy d-flex flex-column flex-grow-1 justify-content-between">
 		                    	<div class="text">
 		                        	<h2><a href="<?= get_permalink(); ?>" class="stretched-link"><?php the_title(); ?></a></h2>
-		                        	<h3 class="author"><i>by <a href="#"><?php the_author(); ?></a></i></h3>
+		                        	<h3 class="author"><i>by <?php the_author(); ?></i></h3>
 		                        	<p><?= get_the_excerpt(); ?></p>
 		                       </div>
 		                       <button class="align-items-center cta d-flex justify-content-center lightblue text-uppercase text-white mt-4">read more</button>
