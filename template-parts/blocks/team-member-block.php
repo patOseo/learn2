0fe40b385688db22285781bc61fd8ff4eea16d2a
Pat Monette <patrick@ontarioseo.ca>
2021-09-23 20:18:34 +0000

    our team page update

--- template-parts/blocks/team-member-block.php
+++ template-parts/blocks/team-member-block.php
@@ -15,10 +15,13 @@
 //     $className .= ' align' . $block['align'];
 // }
 
+$cols = get_field('columns');
+
 $staffargs = array(
   'post_type' => 'staff',
   'posts_per_page' => -1,
   'order' => 'ASC',
+  'post__not_in' => array(4448),
 );
 
 $staff = new WP_Query($staffargs);
@@ -41,7 +44,7 @@ $staff = new WP_Query($staffargs);
 
               ?>
                    
-                     <div class="col-md-4">
+                     <div class="col-md-<?php echo $cols; ?>">
                        <div class="align-items-center box d-flex flex-column justify-content-center <?= $color; ?>">
                            <a href="<?php the_permalink(); ?>">
                             <div class="img"><?php echo wp_get_attachment_image($img, 'square', '', array('class' => 'w-100 h-auto')); ?></div>
@@ -64,6 +67,51 @@ $staff = new WP_Query($staffargs);
                    </div>
 
               <?php endwhile; ?>
+
+
+
+
+              <?php 
+
+              // Display Pepper as the last Team Member
+              $pepper = 4448; ?>
+              <?php if(get_post($pepper)): ?>
+                
+                <div class="col-md-<?php echo $cols; ?>">
+
+                  <?php
+                    $title = get_the_title($pepper);
+                    $bio = get_field('bio', $pepper);
+                    $img = get_field('profile_image', $pepper);
+                    $position = get_field('position', $pepper);
+                    $email = get_field('email', $pepper);
+                    $color = get_field('natural_approach', $pepper);
+                  ?>
+                    <div class="align-items-center box d-flex flex-column justify-content-center <?= $color; ?>">
+                        <a href="<?php the_permalink($pepper); ?>">
+                         <div class="img"><?php echo wp_get_attachment_image($img, 'square', '', array('class' => 'w-100 h-auto')); ?></div>
+                        </a>
+                        <div class="accordion">
+                            <div class="copy-acc">
+                                <p><?= $bio; ?></p>
+                            </div>
+                            <div class="d-flex icon justify-content-center">
+                                <span class="plus"></span>
+                                <span class="minus"></span>
+                            </div>
+                        </div>
+                        <div class="meta text-center">
+                            <h2><?= $title; ?></h2>
+                            <p><?= $position; ?></p>
+                            <a href="mailto:<?= $email; ?>"><?= $email; ?></a>
+                        </div>
+                    </div>
+                </div>
+                
+
+              <?php endif; ?>
+
+
         </div>
     </div>
 </section>
