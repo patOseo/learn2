214c2422e88706c472b4dc1e9f311d659fbd01ac
Pat Monette <patrick@ontarioseo.ca>
2021-11-12 19:47:54 +0000

    fix cta on oa block

--- template-parts/blocks/orchestrate-footer-block.php
+++ template-parts/blocks/orchestrate-footer-block.php
@@ -12,6 +12,6 @@ $button_link = get_field('button_link');
     <section class="align-items-center d-flex hero justify-content-center publicity-hero bottom" style="background-image: url('<?= $background_image; ?>');">
         <div class="container text-center">
             <h2 class="text-uppercase"><?= $heading; ?></h2>
-            <a href="<?= $button_link; ?>" class="cta d-inline-flex mt-4 mt-md-5 red-white red-white-color shadow white-red-text"><?= $button; ?></a>
+            <button data-toggle="modal" data-target="#contactModal" class="cta d-inline-flex mt-4 mt-md-5 red-white red-white-color shadow white-red-text"><?= $button; ?></button>
         </div>
     </section>
