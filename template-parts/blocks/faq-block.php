ce15e59ad0ab5ad4b4f4c07f2a5456221e11eb7a
Pat Monette <patrick@ontarioseo.ca>
2021-12-24 15:43:04 +0000

    faq block html tag update

--- template-parts/blocks/faq-block.php
+++ template-parts/blocks/faq-block.php
@@ -1,3 +1,9 @@
+<?php 
+
+$qtag = get_field('q_tag');
+
+?>
+
 <?php if(have_rows('faq')): ?>
 <section class="sec accordions">
 	<div class="container">
@@ -7,7 +13,14 @@
 					<?php $question = get_sub_field('question'); $answer = get_sub_field('answer'); ?>
 					<div class="acc-box">
             		    <div class="acc-head align-items-center d-flex">
-            		        <h4><?= $question; ?></h4><span class="arrow"></span>
+            		    	<?php if($qtag == 'h2'): ?>
+            		        	<h2 class="h4"><?= $question; ?></h2>
+            		        <?php elseif($qtag == 'h3'): ?>
+            		        	<h3 class="h4"><?= $question; ?></h3>
+            		        <?php else: ?>
+            		        	<h4><?= $question; ?></h4>
+            		        <?php endif; ?>
+            		        <span class="arrow"></span>
             		    </div>
             		    <div class="acc-content">
             		        <p><?= $answer; ?></p>
