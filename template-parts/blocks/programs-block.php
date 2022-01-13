7f19257c00d99cfb885417e817f9dacaae78f2ab
Pat Monette <patrick@ontarioseo.ca>
2022-01-13 16:40:31 +0000

    programs block update

--- template-parts/blocks/programs-block.php
+++ template-parts/blocks/programs-block.php
@@ -1,6 +1,7 @@
 <?php
 
 $manual = get_field('manual');
+$featured_view = get_field('featured_programs_view');
 
 $the_progs = null;
 $the_cat = null;
@@ -54,22 +55,42 @@ $programs = new WP_Query($args);
 ?>
 
 <?php if($programs->have_posts()): ?>
-<section class="sec callsec programsec">
-    <div class="row">
-        <?php while($programs->have_posts()): $programs->the_post(); ?>
-            <div class="col-md-4">
-                <div class="box d-flex flex-column h-100 justify-content-between">
-                    <div class="img"><img src="<?php echo the_post_thumbnail_url('program-view'); ?>" alt="<?php the_title(); ?>" class="w-100"></div>
-                    <div class="copy d-flex flex-column flex-grow-1 justify-content-between">
-                        <div class="text">
-                            <h2><a href="<?php the_permalink(); ?>" class="stretched-link"><?php the_title(); ?></a></h2>
-                            <p><?php echo wp_trim_words(the_excerpt(), 30); ?></p>
+    <?php if($featured_view): ?>
+        <section class="jumbo sec">
+            <div class="promoted-link container-xl">
+                <div class="row justify-content-center">
+                    <?php while($programs->have_posts()): $programs->the_post(); ?>
+                    <div class="col-md-6 px-0">
+                        <div class="align-items-center box d-flex flex-column h-100 justify-content-between text-center" style="background: linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.5)), url('<?php echo the_post_thumbnail_url('program-view'); ?>');background-size:cover;background-repeat:no-repeat;background-position:center center;">
+                            <div class="copy flex-grow-1">
+                                <h2><?php the_title(); ?></h2>
+                                <p><?php echo wp_trim_words(the_excerpt(), 30); ?></p>
+                            </div>
+                            <a href="<?php the_permalink(); ?>" class="align-items-center blue cta d-inline-flex mt-4 text-uppercase text-white" target="_blank" rel="noopener,noreferrer">Learn More</a>
                         </div>
-                        <button class="align-items-center cta d-flex justify-content-center lightblue text-uppercase text-white mt-4">read more</button>
                     </div>
+                    <?php endwhile; ?>
                 </div>
             </div>
-        <?php endwhile; ?>
-    </div>
-</section>
+        </section>
+    <?php else: ?>
+        <section class="sec callsec programsec">
+            <div class="row">
+                <?php while($programs->have_posts()): $programs->the_post(); ?>
+                    <div class="col-md-4">
+                        <div class="box d-flex flex-column h-100 justify-content-between">
+                            <div class="img"><img src="<?php echo the_post_thumbnail_url('program-view'); ?>" alt="<?php the_title(); ?>" class="w-100"></div>
+                            <div class="copy d-flex flex-column flex-grow-1 justify-content-between">
+                                <div class="text">
+                                    <h2><a href="<?php the_permalink(); ?>" class="stretched-link"><?php the_title(); ?></a></h2>
+                                    <p><?php echo wp_trim_words(the_excerpt(), 30); ?></p>
+                                </div>
+                                <button class="align-items-center cta d-flex justify-content-center lightblue text-uppercase text-white mt-4">read more</button>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        </section>
+    <?php endif; ?>
 <?php endif; ?>
