9cc05e4199acfa333308559c8257329b7144af69
Pat Monette <patrick@ontarioseo.ca>
2022-02-02 18:51:36 +0000

    recent posts block update

--- template-parts/blocks/recent-posts-block.php
+++ template-parts/blocks/recent-posts-block.php
@@ -16,10 +16,8 @@
         <div class="row">
             <div class="box col-md-4 d-flex flex-column justify-content-between">
                 <div class="upper">
-                    <?php $image_src = get_the_post_thumbnail_url($recent_post_1, 'medium'); ?>
                     <div class="img-place">
-                        
-                        <img src="<?= $image_src; ?>" alt="" class="w-100">
+                        <?php echo get_the_post_thumbnail($recent_post_1, 'medium', array('class' => 'w-100 h-auto')); ?>
                     </div>
                     <h3 class="text-uppercase"><a href="<?= get_permalink($recent_post_1); ?>"><?= get_the_title($recent_post_1); ?></a></h3>
                     <p><?= get_the_excerpt($recent_post_1);  ?></p>
@@ -28,9 +26,8 @@
             </div>
             <div class="box col-md-4 d-flex flex-column justify-content-between">
                 <div class="upper">
-                     <?php $image_src2 = get_the_post_thumbnail_url($recent_post_2, 'medium'); ?>
                         <div class="img-place">
-                            <a href="#"><img src="<?= $image_src2; ?>" alt="" class="w-100"></a>
+                            <?php echo get_the_post_thumbnail($recent_post_2, 'medium', array('class' => 'w-100 h-auto')); ?>
                         </div>
                     <h3 class="text-uppercase"><a href="<?= get_permalink($recent_post_2); ?>"><?= get_the_title($recent_post_2); ?></a></h3>
                     <p><?= get_the_excerpt($recent_post_2);  ?></p>
@@ -40,8 +37,7 @@
             <div class="box col-md-4 d-flex flex-column justify-content-between">
                 <div class="upper">
                     <div class="img-place">
-                            <?php $image_src3 = get_the_post_thumbnail_url($recent_post_3, 'medium'); ?>
-                            <a href="#"><img src="<?= $image_src3; ?>" alt="" class="w-100"></a>
+                            <?php echo get_the_post_thumbnail($recent_post_3, 'medium', array('class' => 'w-100 h-auto')); ?>
                         </div>
                     <h3 class="text-uppercase"><a href="<?= get_permalink($recent_post_3); ?>"><?= get_the_title($recent_post_3); ?></a></h3>
                     <p><?= get_the_excerpt($recent_post_3);  ?></p>
