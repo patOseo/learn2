8bb7719a3d7af83950a257cc95695156e8a796eb
Pat Monette <patrick@ontarioseo.ca>
2022-02-11 18:53:28 +0000

    modal link block update

--- template-parts/blocks/modal-link-block.php
+++ template-parts/blocks/modal-link-block.php
@@ -11,11 +11,12 @@ $color = get_field('button_color');
 	<?php while(have_rows('modal_link')): the_row(); ?>
 		<?php 
 			$link = get_sub_field('page_link');
+			$modal = get_sub_field('modal');
 			$base_url = esc_url(home_url( '/' ));
 			$str_link = str_replace($base_url, '', $link);
 		?>
 		<div class="wp-block-button mb-1">
-			<a class="wp-block-button__link modal-popup <?php echo $color; ?>" <?php if($link): ?>data-page="<?php echo str_replace('/', '', $str_link); ?>" href="<?php echo $link; ?>"<?php endif; ?>><?php the_sub_field('button_text'); ?></a>
+			<a class="wp-block-button__link <?php if($modal == 1) { echo "modal-popup"; } ?> <?php echo $color; ?>" <?php if($modal == 1) { ?>data-page="<?php echo str_replace('/', '', $str_link); ?>"<?php } ?> href="<?php echo $link; ?>"><?php the_sub_field('button_text'); ?></a>
 		</div>
 	<?php endwhile; ?>
 	</div>
