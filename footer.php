448d58196f0cf71edf139328e6bd8fd2dba1b3ba
Pat Monette <patrick@ontarioseo.ca>
2022-02-17 16:22:15 +0000

    defer conversations widget script

--- footer.php
+++ footer.php
@@ -112,7 +112,7 @@
 
 <?php wp_footer(); ?>
 
-<script type="text/javascript">
+<script type="text/javascript" defer>
     (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
     vgo('setAccount', '649628025');
     vgo('setTrackByDefault', true);
