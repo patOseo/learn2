cb1c93bce3e971c97ef032d98a66fab7d26d3d5a
Pat Monette <patrick@ontarioseo.ca>
2022-03-10 14:12:21 +0000

    add main content to blog template

--- blog.php
+++ blog.php
@@ -18,6 +18,13 @@ get_header();
 ?>
 
 
+<div class="container">
+	<div class="row">
+		<div class="col-12">
+			<?php the_content(); ?>
+		</div>
+	</div>
+</div>
 
 <section class="sec blogsec">
     <div class="container">
