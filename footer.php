fd92a48d5444eee8c7a1eab16e19b10c21f8b618
Pat Monette <patrick@ontarioseo.ca>
2022-03-22 19:29:12 +0000

    set timeout on chat widget

--- footer.php
+++ footer.php
@@ -113,11 +113,20 @@
 <?php wp_footer(); ?>
 
 
+<!--
 <script type="text/javascript" defer>
     (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
     vgo('setAccount', '649628025');
     vgo('setTrackByDefault', true);
     vgo('process');
+</script>
+-->
+
+<script type="text/javascript" defer>
+    var _x = function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)
+    }
+
+    setTimeout(function() { _x(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");vgo('setAccount', '649628025');vgo('setTrackByDefault', true);vgo('process');}, 4000);
 </script>
 
 <noscript>
@@ -126,5 +135,4 @@
 
 </body>
 </html>
-
 
