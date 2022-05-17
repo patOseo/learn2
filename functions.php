8d569675174c7a8125082651a7a8362526d7835d
Pat Monette <patrick@ontarioseo.ca>
2022-05-17 13:07:38 +0000

    fix search issue

--- functions.php
+++ functions.php
@@ -1006,13 +1006,16 @@ add_action('wp_ajax_nopriv_filter_blogs', 'filter_blogs');
 
 
 // Disable Search Pages
+
 function wpb_filter_query( $query, $error = true ) {
-    if ( is_search() ) {
-        $query->is_search = false;
-        $query->query_vars[s] = false;
-        $query->query[s] = false;
-    if ( $error == true )
-        $query->is_404 = true;
+    if(!is_admin()) {
+        if ( is_search() ) {
+            $query->is_search = false;
+            $query->query_vars[s] = false;
+            $query->query[s] = false;
+        if ( $error == true )
+            $query->is_404 = true;
+        }  
     }
 }
 add_action( 'parse_query', 'wpb_filter_query' );
